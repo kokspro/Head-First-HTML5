00d75f4dc8997d62d4a99a8e9399f181ead8dcb1
kokspro <koks.programming@gmail.com>
2022-08-30 12:22:22 -0500

    Starting Head First HTML5 Book

---
+++ chapter2.js
@@ -0,0 +1,12 @@
+// num = "3" + "4";
+// num1 = "3" * "4";
+
+// alert(num);
+// alert(num1);
+
+var count = 0;
+for (let i = 0; i < 5; i++) {
+    count = count + i;
+}
+
+alert(`Count is ${count}`);
