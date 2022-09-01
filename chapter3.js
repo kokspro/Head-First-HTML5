d2e12951bd73f9d3b4b435e453bcd90200ff7af1
kokspro <koks.programming@gmail.com>
2022-09-01 12:38:45 -0500

    Added working remove button to li elements

--- chapter3.js
+++ chapter3.js
@@ -15,12 +15,21 @@ function handleButtonClick() {
     } else {
         var li = document.createElement("li");
         var ul = document.getElementById("playlist");
+        var rmvBtn = document.createElement("button");
 
+        rmvBtn.innerHTML = "Remove";
         input.value = "";
         li.innerHTML = songName;
         ul.appendChild(li);
+        li.appendChild(rmvBtn);
         // save(songName);
+
+        rmvBtn.addEventListener('click', removeIt);
     }
+}
+
+function removeIt() {
+    this.parentElement.remove();
 }
 
 
